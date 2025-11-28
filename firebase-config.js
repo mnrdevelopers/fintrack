3772f0239bb9df9aafad316b370db53e6c44c62f
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-11-28 08:26:56 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,13 +9,6 @@ const firebaseConfig = {
   measurementId: "G-F2RS4TFYPS"
 };
 
-// Initialize Firebase
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-export { auth, db };
